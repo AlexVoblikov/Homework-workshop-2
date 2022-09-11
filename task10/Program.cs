5c2c248fd5bd416e9d2b8a8f30ba747061a2d0f1
Alexey Voblikov <1nro@bk.ru>
2022-09-11 14:40:33 +0400

    создали реп

---
+++ task10/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача № 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.WriteLine("Введите трехзначный номер: ");
+
+int digit = Convert.ToInt32(Console.ReadLine());
+
+if (digit < 100 || digit > 999) 
+{
+Console.WriteLine("Вы ввели не 3 значное число");
+return;
+}
+
+
+int digitFirst = digit / 10;
+int digitSecond = digitFirst % 10;
+Console.WriteLine($"{digitSecond}");
+
+
---
+++ task13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача № 13.Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите трехзначный номер: ");
+
+int digit = Convert.ToInt32(Console.ReadLine());
+
+if (digit < 100 || digit > 999) 
+{
+Console.WriteLine("Вы ввели не 3 значное число");
+return;
+}
+
+
+int digitFirst = digit / 100;
+int digitSecond = digit % 10;
+Console.WriteLine($"{digitSecond}");
